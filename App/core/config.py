a56bfc526cb52d6741e18d0a53b9028803631ab0
Carles Perosanz <carlespegu@gmail.com>
2026-03-29 19:17:51 +0200

    change config.py HS256

--- App/core/config.py
+++ App/core/config.py
@@ -9,8 +9,8 @@ class Settings:
     APP_VERSION: str = os.getenv("APP_VERSION", "2.0.0")
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
 
-    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "change-this-in-env")
-    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES", "60")
     )
